be6d315a03293abbfbb6412fc5a11abbbccb0c92
Andressa Rocha <andressarochamorgado@hotmail.com>
2017-05-27 17:32:08 +0000

    Modificando a css, deletando a pagina doar.html, pois a dor e receber serão realizadas na mesma pagina htlm, criando doacao.js com a função de Doar() , criando um vetor que que armazena uma pilha de objetos relacionadas aos pacientes que precisam de doação de sangue, também foi inserida a função geraTabela() que retorna os pacientes cadastrados no vetor anterior

--- js/bd.js
+++ js/bd.js
@@ -32,3 +32,43 @@ function criarTabelaHemonucleo(){
         };
     }   
 }
+
+function getDB(){ 
+	var open = indexedDB.open("Hemonucleo", 1);
+	open.onsuccess = function() {
+		var db = open.result;
+		var tx = db.transaction("MyObjectStore", "readwrite");
+		var store = tx.objectStore("MyObjectStore");
+		var index = store.index("NameIndex");
+		var getHospital1 = store.get(1);
+		var getHospital2 = store.get(2);
+        var getHospital1 = store.get(1);
+		var getHospital2 = store.get(2);
+		
+		var tabela = document.getElementById("tb1");
+		var tr = document.createElement("tr");
+		var tr2 = document.createElement("tr");
+		var tdHospital = document.createElement("td");
+		var tdHospital2 = document.createElement("td");
+		var tbody = document.querySelector("tbody");
+        
+        //recebe os valores passados no bd.js para as tds
+		getHospital1.onsuccess = function() {
+			tdHospital.innerHTML = "<br>" + getHospital1.result.name + " <br> " + getHospital1.result.description + "<br>" + getHospital1.result.code;
+		};
+		
+		getHospital2.onsuccess = function() {
+			tdHospital2.innerHTML = "<br>" + getHospital2.result.name + " <br> " + getHospital2.result.description + "<br>" + getHospital2.result.code;
+		};
+
+    	tr.appendChild(tdHospital);
+    	tr2.appendChild(tdHospital2);
+    	tbody.appendChild(tr);
+    	tbody.appendChild(tr2);
+    	tabela.appendChild(tbody);
+    		
+    	tx.oncomplete = function() {
+    		db.close();
+    	};
+	}
+}
