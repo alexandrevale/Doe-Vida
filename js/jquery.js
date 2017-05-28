8ab7c3d8b0be0807b68602bf61f0532ae4522d06
Andressa Rocha <andressarochamorgado@hotmail.com>
2017-05-28 04:32:07 +0000

    Criando arquivo jquery que habilita div

---
+++ js/jquery.js
@@ -0,0 +1,12 @@
+$(main)
+
+function main (){
+ $( "#Clique" ).click(function() {
+         $("#conteudo1").css("display","block");
+    });
+    
+ $( "#Clique2" ).click(function() {
+         $("#conteudo2").css("display","block");
+    });
+    
+}
